ff16ba1e20b575517d22684901eb143b1168a855
Daniel Conlon <danielcconlon@gmail.com>
2022-04-24 19:03:37 -0400

    added more robots to the array to fight

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,17 +1,22 @@
 var playerName = window.prompt("What is your robot's name?")
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 //You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth)
+//console.log(playerName, playerAttack, playerHealth)
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var playerMoney = 10;
 
-var fight = function() {
+console.log(enemyNames);
+console.log(enemyNames.length);
+console.log(enemyNames[0]);
+console.log(enemyNames[3]);
+
+var fight = function(enemyName) {
     //Alert players that they are starting the round
     window.alert("Welcome to the Robot Gladiators");
 
@@ -75,4 +80,7 @@ var fight = function() {
 
 };
 
-fight();
+//fight();
+for(var i = 0; i < enemyNames.length; i++){
+    fight(enemyNames[i]);
+}
