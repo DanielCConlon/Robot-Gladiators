9f4e2ba055a87215d6e157042d616f3bca2b45cc
Daniel Conlon <danielcconlon@gmail.com>
2022-04-24 22:24:50 -0400

    added improvement to the game experience

--- assets/js/game.js
+++ assets/js/game.js
@@ -77,19 +77,38 @@ var fight = function(enemyName) {
         else{
             window.alert(playerName + " still has " + playerHealth + " health left.");
         }
-        
+
     }// end of while loop
 
 }; //end of fight function
 
 //fight();
 for(var i = 0; i < enemyNames.length; i++){
-    //debugger;
-    //pick new enemy to fight based on the index of the enemyNames array
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
 
-    //call fight function with enemy-robot
+    //if player is still alive, keep fighting
+    if (playerHealth > 0){
+        //let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+        window.alert("Welcome to Robot Galdiators! Round " + (i + 1));
+
+        //pick new enemy to fight based on the index of the enemyNames array
+        var pickedEnemyName = enemyNames[i];
+
+        //reset enemyHealth before starting new fight
+        enemyHealth = 50;
+
+        //use debugger to pause script from running and check what's going on at the moment in code
+        //debugger;
+        
+        //pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+            //call fight function with enemy-robot
 
     fight(pickedEnemyName);
-}
+    }
+
+    //if player isn't alive, stop the game
+    else{
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+    };//end of the if statement
+
+} //end of for loop
